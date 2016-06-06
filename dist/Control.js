833a9f15ab73536d0fb4a567a90c17bb98e3b780
Augusto Monteiro <augustomna2010@gmail.com>
2016-06-05 23:34:14 -0300

    Updating

--- dist/Control.js
+++ dist/Control.js
@@ -2,6 +2,8 @@
 
 var Control = {
 
+    content: document.getElementById("Content"),
+
     html: "<div id=\"MediaControls\">\n    <button onclick=\"conn.send({action: 'play'});\">PLAY</button>\n    <button onclick=\"conn.send({action: 'pause'});\">PAUSE</button>\n    <input id=\"Volume\" oninput=\"conn.send({action: 'volume', data: this.value});\" type=\"range\" min=\"0\" max=\"100\"/>\n    <input type=\"text\" id=\"SearchInput\" placeholder=\"Search\" onkeydown=\"if (event.keyCode === 13) {Control.search();}\"/>\n    </div>\n    <ul id=\"Videos\">\n\n    </ul>",
 
     createMediaControls: function createMediaControls() {},
@@ -12,18 +14,21 @@ var Control = {
         document.querySelector("#SearchInput").blur();
     },
     renderVideosList: function renderVideosList(list) {
-        var ul = document.querySelector("#Videos");
-        ul.innerHTML = "";
+        var ul = document.querySelector("#Videos"),
+            fragment = document.createDocumentFragment();
+
         list.items.forEach(function (item) {
             var li = document.createElement("li");
             li.innerHTML = "<img class='thumb' src='" + item.snippet.thumbnails.default.url + "'>\n                <h3>" + item.snippet.title + "</h3>";
             li.onclick = function () {
                 return conn.send({ action: 'open', data: item.id.videoId });
             };
-            ul.appendChild(li);
+            fragment.appendChild(li);
         });
+        ul.innerHTML = "";
+        ul.appendChild(fragment);
     },
     render: function render() {
-        document.getElementById("Content").innerHTML = this.html;
+        this.content.innerHTML = this.html;
     }
 };
--- dist/Player.js
+++ dist/Player.js
@@ -2,21 +2,18 @@
 
 var Player = {
 
+    content: document.getElementById("Content"),
     youtubeFrameLoaded: false,
 
-    html: "<div id=\"Config\">\n                <img id=\"QRCode\" src=\"\">\n           </div>",
-
     showPlayer: function showPlayer() {
-        document.getElementById("Content").innerHTML = "<div id=\"Player\"></div>";
-
         if (!this.youtubeFrameLoaded) {
+            document.getElementById("Content").innerHTML = "<div id=\"Player\"></div>";
             loadIframeApi();
             this.youtubeFrameLoaded = true;
-        } else {
-            onYouTubeIframeAPIReady();
         }
     },
     render: function render() {
         document.getElementById("Content").innerHTML = this.html;
+        this.content.innerHTML = "<div id=\"Config\">\n            <img id=\"QRCode\" src=\"\">\n       </div>";
     }
 };
